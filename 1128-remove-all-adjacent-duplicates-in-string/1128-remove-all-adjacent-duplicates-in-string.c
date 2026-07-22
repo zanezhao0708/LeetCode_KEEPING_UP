2e6e6e525c8a18100179b17dbef7557110e38566
Zane Zhao <zeruizhao78@gmail.com>
2026-07-22 22:00:06 +0800

    Time: 0 ms (100%), Space: 10.6 MB (46.09%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.c
@@ -0,0 +1,22 @@
+char* removeDuplicates(char* s) {
+    int n = strlen(s);//获取字符串长度
+    if(n == 0)return "";
+    if(n == 1)return s;
+
+    int top = 0;//此时top永远指向下一个入栈道的位置（是空的）
+    //由于这种设置
+    
+    char* stack =(char*)malloc(sizeof(char) * (n+1));//给'\0'留空间
+    for (int i = 0; i < n; i++) {
+        if (top != 0 && s[i] == stack[top-1]) { // 当前遍历到的元素与刚刚入栈元素相同进行删除
+        
+            top--;
+            }
+        else{
+                stack[top++] = s[i];
+            }
+        }
+    
+    stack[top]='\0';//字符串结束符
+    return stack;
+}
