728fcf7f6af9c1be225e8aba1bb2c963e02e8637
Zane Zhao <zeruizhao78@gmail.com>
2026-07-23 00:32:42 +0800

    Time: 69 ms (57.56%), Space: 23 MB (68.34%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
@@ -10,10 +10,11 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     struct ListNode *cur2 = headB;
     int count = 0;
     int flag = -1;
-    while(cur1->next !=NULL && cur2->next){
+    while(cur1->next !=NULL && cur2->next){//两个指针同时前进直到一个走到尽头
         cur1 = cur1->next;
         cur2 = cur2->next;
     }
+    //查看一个快指针比慢指针 快多少步
     while(cur2->next != NULL){
         flag = 1;
         cur2 = cur2->next;
@@ -26,15 +27,14 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     }
     cur1 = headA;
     cur2 = headB;
-    while(count--){
+    while(count--){//补齐差距
         if(flag)cur2 = cur2->next;
         else cur1 = cur1->next; }
 
-    while(cur1!=cur2){
+    while(cur1!=cur2){//当指针相同时即所要的节点
         if(cur1 == NULL || cur2 == NULL) return NULL;
         cur1 = cur1->next;
         cur2 = cur2->next;
     }
     return cur1;
-
 }
