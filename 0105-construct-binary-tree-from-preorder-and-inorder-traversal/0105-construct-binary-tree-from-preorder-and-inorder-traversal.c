3a0157a24d0c036594b5aada426e1f0e10ee98f4
Zane Zhao <zeruizhao78@gmail.com>
2026-07-23 17:43:40 +0800

    Time: 7 ms (82.55%), Space: 14.6 MB (94.18%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -14,7 +14,7 @@ struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int in
     for(id = 0;id < inorderSize;id++){
         if(inorder[id] == preorder[0])break;
     }
-    tree->left = buildTree(preorder+1,id,inorder,id);
+    tree->left = buildTree(preorder+1,id,inorder,id);//注意前面区间长度是id
     tree->right = buildTree(preorder+ id +1,inorderSize- id -1,inorder+ id + 1,inorderSize-id-1);
     return tree;
 }
