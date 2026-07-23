523b8ded6414bf44aabaad72011cdf1998ac7b4b
Zane Zhao <zeruizhao78@gmail.com>
2026-07-23 19:44:32 +0800

    Time: 0 ms (100%), Space: 8.5 MB (57.47%) - LeetHub

--- 100345-qiu-12n-lcof/100345-qiu-12n-lcof.c
+++ 100345-qiu-12n-lcof/100345-qiu-12n-lcof.c
@@ -1,4 +1,5 @@
 int mechanicalAccumulator(int target) {
-    if(target == 1) return 1;
-    return target +mechanicalAccumulator(target - 1);
+    int sum = 0;
+    target && (sum = target + mechanicalAccumulator(target - 1));//利用短路，有0元素后结果为0
+    return sum;
 }
