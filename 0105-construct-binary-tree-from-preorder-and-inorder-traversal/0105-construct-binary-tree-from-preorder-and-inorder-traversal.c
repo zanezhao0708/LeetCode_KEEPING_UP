9f066024d7fd1c14c9e0b119544e54e83d60b382
Zane Zhao <zeruizhao78@gmail.com>
2026-07-23 17:42:55 +0800

    Time: 19 ms (5.82%), Space: 14.6 MB (92.94%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize) {
+    if(preorderSize == 0)return NULL;//递归出口
+    struct TreeNode* tree = (struct TreeNode* )malloc(sizeof(struct TreeNode));
+    tree->val = preorder[0];//前序序列的第一个元素必然为根节点的值
+    int id;
+    for(id = 0;id < inorderSize;id++){
+        if(inorder[id] == preorder[0])break;
+    }
+    tree->left = buildTree(preorder+1,id,inorder,id);
+    tree->right = buildTree(preorder+ id +1,inorderSize- id -1,inorder+ id + 1,inorderSize-id-1);
+    return tree;
+}
