cebc70af150eab4d29e6089b11d52b5658243277
Zane Zhao <zeruizhao78@gmail.com>
2026-07-23 16:55:30 +0800

    Time: 11 ms (74.07%), Space: 11.7 MB (82.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -7,11 +7,15 @@
  */
 bool hasCycle(struct ListNode *head) {
     if(!head)return false;
-    int count = 10000;
-    while(count){
-        if(head->next == NULL)return false;
-        head = head->next;
-        count--;
+    //利用快慢指针：快指针每次走两步，慢指针每次走一步，若有环最终必定相遇
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while(true){
+        if (fast == NULL || fast->next == NULL) 
+        return false;//链表有尾，必然没有循环
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast == slow)return true;//找到循环点位
+        
     }
-    return true;
 }
