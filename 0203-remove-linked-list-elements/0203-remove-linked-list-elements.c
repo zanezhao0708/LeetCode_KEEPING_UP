75d3acad06a67e511002652ea1b1107c474fc21f
Zane Zhao <zeruizhao78@gmail.com>
2026-07-25 20:52:59 +0800

    Time: 4 ms (12.78%), Space: 13.2 MB (30.49%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
@@ -9,20 +9,16 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     struct ListNode* L = (struct ListNode*)malloc(sizeof(struct ListNode));
     L->next = head;
 
-    struct ListNode* front = L;
-    struct ListNode* rear = head;
+    struct ListNode* cur = L;
+    while(cur->next){
+        struct ListNode* nxt = cur ->next;
+        if(nxt -> val == val){//删除节点nxt
+            cur->next = nxt->next;
+            free(nxt);
+        }
+        else cur = nxt;
 
-    while(rear){
-        if(rear->val == val){
-            front->next = rear->next;
-            rear = rear -> next;
-            //注意此时front指针不用移动
-            //因为假设有连续要删除的变量，就会导致有应当删除节点的残留
-        }
-        else{
-            front = front -> next;
-            rear = rear -> next; 
-        }
-    } 
+
+    }
     return L->next;
 }
