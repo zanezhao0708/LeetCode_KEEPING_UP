aad379bc2610529df7b0264db5861fd74862718f
Zane Zhao <zeruizhao78@gmail.com>
2026-08-08 23:22:04 +0800

    Time: 0 ms (100%), Space: 13.7 MB (31.53%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
@@ -17,17 +17,23 @@ int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes
     *returnSize = 0;
 
     if(root == NULL) return ans;//若树为空，直接返回ans
+    
     struct TreeNode* queue[2000];
     int head = 0, tail = 0;
     queue[tail++] = root;
     // 结束条件为队列为空，即tail==head
     while(head != tail){
+        
         int len = tail - head;
         ans[*returnSize] = malloc(sizeof(int) * len); // 开辟当前层的一维数组空间
+        
         int start = head;
         head = tail;// start被赋值后变为当前层的头部，head被赋值后变为当前层的尾部
+        
         for(int i = start; i < head; i++){// 逐个访问当前层的节点值
-            ans[*returnSize][i - start] = queue[i]->val;// 访问完一个节点后，将此节点的左右孩子入队
+            ans[*returnSize][i - start] = queue[i]->val;//i和start都是在原树结构中的整体队列
+            //利用i-start相对位置构造第二维
+            // 访问完一个节点后，将此节点的左右孩子入队
             if(queue[i]->left) queue[tail++] = queue[i]->left;
             if(queue[i]->right) queue[tail++] = queue[i]->right;
         }
