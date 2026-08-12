05f75edcb1e44bf9210a7e5e50b9d6a2fe50ba2c
Zane Zhao <zeruizhao78@gmail.com>
2026-08-12 17:27:53 +0800

    Time: 3 ms (15.31%), Space: 19.8 MB (5.03%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* rightSideView(struct TreeNode* root, int* returnSize) {
+    *returnSize = 0;
+    int *ans = malloc( sizeof(int) * 2000);
+    if(!root)return ans;
+
+    int rear = 0,front = 0;
+    struct TreeNode* queue[1000000];
+
+    queue[rear++] = root;
+    while(front < rear){
+        int len = rear - front;
+        int last = rear - 1;
+        for(int i = 0;i < len;i++){
+            if(i == len-1)ans[(*returnSize)++] = queue[front]->val;
+            if(queue[front]->left)queue[rear++] = queue[front]->left;
+            if(queue[front]->right)queue[rear++] = queue[front]->right;
+            front++;
+        }
+    }
+    return ans;
+}
