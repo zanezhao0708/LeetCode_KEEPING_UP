e59d3849db72b95bd627c5fb3386a83ae37e063b
Zane Zhao <zeruizhao78@gmail.com>
2026-08-12 15:59:07 +0800

    Time: 0 ms (100%), Space: 13.7 MB (33.04%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
@@ -35,42 +35,8 @@ int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes
             if(queue[i]->left)queue[rear++] = queue[i]->left;
             if(queue[i]->right)queue[rear++] = queue[i]->right;
         }
-        (*returnColumnSizes)[(*returnSize)++] = len;
+        (*returnColumnSizes)[*returnSize] = len;
+        (*returnSize)++;
     }
     return ans;
-
 }
-
-
-// int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
-//     int** ans = (int**)malloc(sizeof(int*) * 2000);//开辟二维数组
-//     *returnColumnSizes = malloc(sizeof(int) * 2000);// *returnColumnSizes数组记录每一层节点的个数
-//     *returnSize = 0;
-
-//     if(root == NULL) return ans;//若树为空，直接返回ans
-    
-//     struct TreeNode* queue[2000];
-//     int head = 0, tail = 0;
-//     queue[tail++] = root;
-//     // 结束条件为队列为空，即tail==head
-//     while(head != tail){
-        
-//         int len = tail - head;
-//         ans[*returnSize] = malloc(sizeof(int) * len); // 开辟当前层的一维数组空间
-        
-//         int start = head;
-//         head = tail;// start被赋值后变为当前层的头部，head被赋值后变为当前层的尾部
-        
-//         for(int i = start; i < head; i++){// 逐个访问当前层的节点值
-//             ans[*returnSize][i - start] = queue[i]->val;//i和start都是在原树结构中的整体队列
-//             //利用i-start相对位置构造第二维
-//             // 访问完一个节点后，将此节点的左右孩子入队
-//             if(queue[i]->left) queue[tail++] = queue[i]->left;
-//             if(queue[i]->right) queue[tail++] = queue[i]->right;
-//         }
-//         // *returnColumnSizes赋值，并将层数加1
-//         (*returnColumnSizes)[(*returnSize)++] = len;
-//     }
-//     return ans;
-
-// }
