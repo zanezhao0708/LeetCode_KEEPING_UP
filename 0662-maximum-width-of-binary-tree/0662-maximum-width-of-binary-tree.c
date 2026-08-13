b07cba05d722ecdc1f11bae019b0e18875a36409
Zane Zhao <zeruizhao78@gmail.com>
2026-08-13 23:15:20 +0800

    Time: 0 ms (100%), Space: 12.5 MB (59.85%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.c
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.c
@@ -9,30 +9,23 @@
 int widthOfBinaryTree(struct TreeNode* root) {
     int front = 0;
     int rear = 0;
-
     struct TreeNode* queue[3000];
     long long index[3000];
-
     queue[rear] = root;
-    index[rear] = 0;
-    rear++;
-
+    index[rear++] = 0;
     int max = 1;
 
     while (front < rear) {
         int len = rear - front;
-
         long long base = index[front];
 
         // 当前层宽度
         long long width = index[rear - 1] - base + 1;
-
         if (width > max)
             max = width;
 
         // 处理当前层的所有节点
         for (int i = 0; i < len; i++) {
-
             long long cur = index[front] - base;
 
             if (queue[front]->left) {
@@ -40,13 +33,11 @@ int widthOfBinaryTree(struct TreeNode* root) {
                 index[rear] = 2 * cur + 1;
                 rear++;
             }
-
             if (queue[front]->right) {
                 queue[rear] = queue[front]->right;
                 index[rear] = 2 * cur + 2;
                 rear++;
             }
-
             front++;
         }
     }
