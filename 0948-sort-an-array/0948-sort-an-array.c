0a9e9545468284aea2b07510437cf8f7d17eda7d
Zane Zhao <zeruizhao78@gmail.com>
2026-08-15 21:14:04 +0800

    Time: 51 ms (65.34%), Space: 57.8 MB (99.21%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.c
@@ -0,0 +1,18 @@
+int* sortArray(int* nums, int numsSize, int* returnSize){
+    *returnSize = numsSize;
+    int step = numsSize/2;
+    while (step > 0) {
+      //!
+      for(int i = step;i<numsSize;++i) {
+        int tmp = nums[i];
+        int j =  i - step;
+        while (j >= 0 && tmp < nums[j]) {
+          nums[j+step] = nums[j];
+          j -= step;
+        }
+        nums[j + step] = tmp;
+      }
+      step = step/2;
+    }
+    return nums;
+}
