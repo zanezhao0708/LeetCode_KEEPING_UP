89a7f761460ad86a809d1c4aab86c97b917cc939
Zane Zhao <zeruizhao78@gmail.com>
2026-08-18 23:40:00 +0800

    Time: 40 ms (5.22%), Space: 23.7 MB (5.21%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.c
+++ 1039-find-the-town-judge/1039-find-the-town-judge.c
@@ -1,10 +1,8 @@
 int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
-    int graph[n + 1][n + 1];
+    int graph[n + 1][n + 1];//注意题目里是从1到n编号
 
     for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= n; j++){
-            graph[i][j] = 0;
-        }
+        for(int j = 0; j <= n; j++)graph[i][j] = 0;//全部初始化为0
     }
 
     for(int i = 0; i < trustSize; i++){
@@ -12,26 +10,15 @@ int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
         int b = trust[i][1];
         graph[a][b] = 1;
     }
-    for(int i = 1; i <= n; i++){
-
+    for(int i = 1; i <= n; i++){//从1到n编号
         int out = 0;
         int in = 0;
 
         for(int j = 1; j <= n; j++){
-
-            if(graph[i][j] == 1){
-                out++;
-            }
-
-            if(graph[j][i] == 1){
-                in++;
-            }
+            if(graph[i][j] == 1)out++;
+            if(graph[j][i] == 1)in++;
         }
-
-        if(out == 0 && in == n - 1){
-            return i;
-        }
+        if(out == 0 && in == n - 1)return i;
     }
-
     return -1;
 }
