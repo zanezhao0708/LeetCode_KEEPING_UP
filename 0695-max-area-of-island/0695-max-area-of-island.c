0a7917d59ce086221605941bd15524daa00f2382
Zane Zhao <zeruizhao78@gmail.com>
2026-08-22 21:58:38 +0800

    Time: 4 ms (18.11%), Space: 11 MB (65.35%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.c
+++ 0695-max-area-of-island/0695-max-area-of-island.c
@@ -1,11 +1,11 @@
-int dfs(int** grid,int rows,int cols,int visited[rows][cols],int row,int col){
+int dfs(int** grid,int rows,int cols,int row,int col){
     if(row >= rows||row<0||col >= cols||col<0 || grid[row][col] != 1)return 0;
     grid[row][col] = 2;
     
-    int left = dfs(grid,rows,cols,visited,row+1,col); 
-    int right =dfs(grid,rows,cols,visited,row-1,col);
-    int up = dfs(grid,rows,cols,visited,row,col+1);
-    int down = dfs(grid,rows,cols,visited,row,col-1);
+    int left = dfs(grid,rows,cols,row+1,col); 
+    int right =dfs(grid,rows,cols,row-1,col);
+    int up = dfs(grid,rows,cols,row,col+1);
+    int down = dfs(grid,rows,cols,row,col-1);
     return 1+left+right+up+down;
 }
 
@@ -13,13 +13,11 @@ int maxAreaOfIsland(int** grid, int gridSize, int* gridColSize) {
     int max = 0;
     int rows  = gridSize;
     int cols = gridColSize[0];
-    int visited[rows][cols];
-
-    for(int i = 0;i<rows;i++)for(int j = 0;j<cols;j++)visited[i][j] = 0;
+   
     for(int i = 0;i<rows;i++){
         for(int j = 0;j<cols;j++){
-            if(visited[i][j] != 1 && grid[i][j] == 1){
-                int area = dfs(grid,rows,cols,visited,i,j);
+            if(grid[i][j] == 1){
+                int area = dfs(grid,rows,cols,i,j);
                 if(area > max)max = area;
             }   
         }
