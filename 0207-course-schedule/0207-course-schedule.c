9c7873dbad42c20b86994897132dfd2709a8dd7a
Zane Zhao <zeruizhao78@gmail.com>
2026-09-04 17:26:37 +0800

    Time: 4 ms (75.14%), Space: 26.4 MB (28.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.c
+++ 0207-course-schedule/0207-course-schedule.c
@@ -1,49 +1,30 @@
 bool canFinish(int numCourses, int** prerequisites,int prerequisitesSize, int* prerequisitesColSize) {
-
-    // graph[i][j] 表示 i 的第 j 个后继课程
     int graph[2000][2000];
-    int outdegree[2000] = {0};
     int indegree[2000] = {0};
+    int outdegree[2000] = {0};
 
-    // 建图
-    for (int i = 0; i < prerequisitesSize; i++) {
-        int course = prerequisites[i][0];
+    for(int i = 0;i<prerequisitesSize;i++){
         int pre = prerequisites[i][1];
+        int course = prerequisites[i][0];
 
-        // pre -> course
         graph[pre][outdegree[pre]++] = course;
         indegree[course]++;
     }
-
-    // 队列，保存当前入度为 0 的节点
+    
     int queue[2000];
     int front = 0;
     int rear = 0;
-
-    for (int i = 0; i < numCourses; i++) {
-        if (indegree[i] == 0) {
-            queue[rear++] = i;
-        }
+    for(int i = 0;i < numCourses;i++){
+        if(indegree[i] == 0)queue[rear++] = i;
     }
 
-    int count = 0;//已处理的节点个数
-
-    while (front < rear) {
+    while(front < rear){
         int cur = queue[front++];
-        count++;
-
-        // 删除 cur 的所有出边
-        for (int i = 0; i < outdegree[cur]; i++) {
-            int next = graph[cur][i];
-
-            indegree[next]--;
-
-            if (indegree[next] == 0) {
-                queue[rear++] = next;
-            }
+        for(int i = 0;i<outdegree[cur];i++){
+            int work = graph[cur][i];
+            indegree[work]--;
+            if(indegree[work] == 0)queue[rear++] = work;
         }
     }
-
-    // 所有课程都能被拓扑排序 => 无环
-    return count == numCourses;
+    return numCourses == rear;
 }
