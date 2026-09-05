e7806232d071bdde632ac5ce800ab222fcbee4dd
Zane Zhao <zeruizhao78@gmail.com>
2026-09-05 11:57:45 +0800

    Time: 4 ms (75.28%), Space: 29.1 MB (21.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.c
+++ 0207-course-schedule/0207-course-schedule.c
@@ -1,30 +1,31 @@
 bool canFinish(int numCourses, int** prerequisites,int prerequisitesSize, int* prerequisitesColSize) {
-    int graph[2000][2000];
-    int indegree[2000] = {0};
-    int outdegree[2000] = {0};
+    int in_degree[2000] = {0};
+    int out_degree[2000] = {0};
+    int path[numCourses][numCourses];
 
     for(int i = 0;i<prerequisitesSize;i++){
         int pre = prerequisites[i][1];
-        int course = prerequisites[i][0];
+        int rear = prerequisites[i][0];
 
-        graph[pre][outdegree[pre]++] = course;
-        indegree[course]++;
+        path[pre][out_degree[pre]++] = rear;
+        in_degree[rear]++;
     }
-    
+
     int queue[2000];
     int front = 0;
     int rear = 0;
-    for(int i = 0;i < numCourses;i++){
-        if(indegree[i] == 0)queue[rear++] = i;
+    for(int i = 0;i<numCourses;i++){
+        if(in_degree[i] == 0)queue[rear++] = i;
     }
 
     while(front < rear){
         int cur = queue[front++];
-        for(int i = 0;i<outdegree[cur];i++){
-            int work = graph[cur][i];
-            indegree[work]--;
-            if(indegree[work] == 0)queue[rear++] = work;
-        }
+        for(int j = 0;j<out_degree[cur];j++){
+            int delete = path[cur][j];
+            in_degree[delete]--;
+            if(in_degree[delete] == 0)queue[rear++] = delete;
+        } 
+
     }
     return numCourses == rear;
 }
