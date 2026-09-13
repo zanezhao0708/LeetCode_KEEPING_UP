34093b78b7573fbd4dc1a0a41aa6398d5d69954a
Zane Zhao <zeruizhao78@gmail.com>
2026-09-13 20:47:35 +0800

    Time: 0 ms (100%), Space: 15.2 MB (83.87%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.c
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <limits.h>
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -6,29 +9,45 @@
  *     struct TreeNode *right;
  * };
  */
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 int* largestValues(struct TreeNode* root, int* returnSize) {
     *returnSize = 0;
-    int* ans = malloc(sizeof(int) *10000);
-    if(!root)return ans;
 
-    int rear = 0,front = 0;
-    struct  TreeNode* queue[10000];
-    queue[rear++] = root;
-    
-    while(front < rear){
-        int max = INT_MIN;
-        int len = rear - front;
-        for(int i = 0;i<len;i++){
-            if(queue[front]->val >max)max = queue[front]->val;
-            if(queue[front]->left)queue[rear++] = queue[front]->left;
-            if(queue[front]->right)queue[rear++] = queue[front]->right;
-            front++;
-        }
-        ans[(*returnSize)++] = max;
-
+    if (root == NULL) {
+        return NULL;
     }
+
+    int* ans = malloc(sizeof(int) * 10000);
+
+    struct TreeNode* queue[10000];
+    int front = 0;
+    int rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        // 当前这一层的结束位置
+        int len = rear;
+        int maxNum = INT_MIN;
+
+        while (front < len) {
+            struct TreeNode* node = queue[front++];
+
+            if (node->val > maxNum) {
+                maxNum = node->val;
+            }
+
+            if (node->left) {
+                queue[rear++] = node->left;
+            }
+
+            if (node->right) {
+                queue[rear++] = node->right;
+            }
+        }
+
+        ans[(*returnSize)++] = maxNum;
+    }
+
     return ans;
 }
