7b3506eed63c594d5a5aa607525aadcd6328ab1c
Zane Zhao <zeruizhao78@gmail.com>
2026-09-14 22:37:35 +0800

    Time: 0 ms (100%), Space: 9.4 MB (22.28%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.c
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.c
@@ -8,7 +8,10 @@
  */
 bool dfs(struct TreeNode* root,int n){
     if(!root)return 1 ;//如果树空表示该路径成功
-    return (root->val == n)&&dfs(root->left,n) && dfs(root->right,n);//所有路径均成功则完成
+    bool L = dfs(root->left ,n);//左
+    bool R = dfs(root->right,n);//右
+    bool res =(root->val == n)&& L &&R;
+    return res;
 
 }
 
