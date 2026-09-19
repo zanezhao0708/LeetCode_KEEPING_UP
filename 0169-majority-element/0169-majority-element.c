c8122c8bf77f21ba4f74500b53ab194d91c4dfbb
Zane Zhao <zeruizhao78@gmail.com>
2026-09-19 20:42:16 +0800

    Time: 0 ms (100%), Space: 11.5 MB (5.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,13 +1,16 @@
 int majorityElement(int* nums, int numsSize) {
-    int ans = 0, n = 0;
-    for (int i = 0; i < numsSize; i++) {
-        int x = nums[i];
-        if (n == 0) {
-            ans = x;
-            n = 1;
-        } else { 
-            n += x == ans ? 1 : -1;
+    int ans = 0;
+    int n = 0;
+    for(int i = 0;i<numsSize;i++){
+        if(n == 0){
+            ans = nums[i];
+            n++;
+        }
+        else{
+            if(nums[i] != ans)n--;
+            else n++;
         }
     }
     return ans;
+    
 }
